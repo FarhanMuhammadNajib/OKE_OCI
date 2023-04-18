240b88dc0c30def95d7fa050e05664d5153d6e29
FarhanMuhammadNajib <farhanmuhammadnajib01@gmail.com>
2023-04-18 14:34:38 +0700

    Update for terraform init

--- Kubernetes/LoadBalancerIpCheck.tf
+++ Kubernetes/LoadBalancerIpCheck.tf
@@ -1,7 +1,33 @@
-#Data source output
+#variable
 variable "compartment_ocid" {
-  default = "ocid1.tenancy.oc1..aaaaaaaa4orlnjmr4z4eaxhrfyxbjm6lgttkfs2hmt2f7x5rrdjvf5ojfebq"
+  
 }
+variable "tenancy_ocid" {
+  
+}
+variable "user_ocid" {
+  
+}
+variable "fingerprint" {
+  
+}
+variable "region" {
+  default = "us-ashburn-1"
+}
+variable "private_key_content" {
+  
+}
+
+#Provider Config
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key      = var.private_key_content
+  region           = var.region
+}
+
+#Data source output
 data "oci_load_balancer_load_balancers" "OKE_load_balancers" {
     #Required
     compartment_id = var.compartment_ocid
